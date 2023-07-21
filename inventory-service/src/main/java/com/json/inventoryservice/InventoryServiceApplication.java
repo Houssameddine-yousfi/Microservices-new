c49423244130acf3006fbcb89aa9fd17036b52aa
Houssameddine Yousfi <sw.houssam@gmail.com>
2023-07-21 14:47:09 +0200

    Adding service discovery server

--- inventory-service/src/main/java/com/json/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/json/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.json.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.json.inventoryservice.model.Inventory;
 import com.json.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/com/json/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/json/orderservice/service/OrderService.java
@@ -21,7 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
     
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -40,8 +40,8 @@ public class OrderService {
 
 
         // check availability in inventory service
-        InventoryResponce[] inventoryResponces = webClient.get()
-            .uri("http://localhost:8082/api/inventory",
+        InventoryResponce[] inventoryResponces = webClientBuilder.build().get()
+            .uri("http://inventory-service/api/inventory",
                     uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
             .retrieve()
             .bodyToMono(InventoryResponce[].class)
