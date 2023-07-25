0dbb4819b6955f517d9ebfd89fca4c66d5e6b62c
Houssameddine Yousfi <sw.houssam@gmail.com>
2023-07-25 17:32:45 +0200

    add resiliancy to the services

--- order-service/src/main/java/com/json/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/json/orderservice/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.json.orderservice.controller;
 
+import java.util.concurrent.CompletableFuture;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.json.orderservice.dto.OrderRequest;
 import com.json.orderservice.service.OrderService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,9 +27,15 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        
-        orderService.placeOrder(orderRequest);
-        return "Order placed Successfully";
+    // the name used in circuitBreaker should be the same as in the application.properties file 
+    @CircuitBreaker(name = "inventory",fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException exception){
+        return CompletableFuture.supplyAsync(() -> "Oops! shomthing went wrong, please try later");
     }
 }
